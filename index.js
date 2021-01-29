42ccd954b2202a45b60ae07daf948c2b8c2f0458
Aleksandar <aleksandar-sokolov@github.com>
2021-01-29 11:44:16 +0200

    adding some views

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const app = express();
 
 const config = require('./config/config')
 
+app.get('/', (req, res) => {
+    res.send('hello world')
+});
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT} ....`));
