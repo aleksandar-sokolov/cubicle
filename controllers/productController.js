ee588c7ca6eeec7f372e260bf32ccb7f62f90ed9
Aleksandar <aleksandar-sokolov@github.com>
2021-01-29 14:14:09 +0200

    Add details page

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,4 +10,8 @@ router.get('/create', (req, res) => {
     res.render('create', {title: 'Create Page'});
 });
 
+router.get('/details/:productId', (req, res) => {
+    res.render('details', {title:  `Details ${req.params.productId}`})
+});
+
 module.exports = router; 
